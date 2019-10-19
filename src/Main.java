8ef9246c6a256634c20a0076460419c354a88278
github username <github email address>
2019-10-19 14:27:50 -0400

    fisrt commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+    int value = 19;
+    int extra = 25;
+    //value <= extra     extra < value      value > -25     value  >=  extra
+    System.out.print(value <= extra);
+    System.out.print(extra < value);
+    System.out.print(value > -25);
+    System.out.print(value  >=  extra);
+
+
+  }
+}
