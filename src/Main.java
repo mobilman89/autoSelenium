0a3b4891b7c274194a33dace9cd980c5ede2fe0d
sochetanie <allagorik2901@gmail.com>
2019-10-22 07:42:13 -0400

    Level 1 - Simple Java Exercises

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,22 @@
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    // 1.Create a loop that will print numbers from 1 to 10, except number 7
+    for (int i = 1; i < 11; i++) {
+      if (i != 7) System.out.println(i);
+    }
 
-    int value = 19;
-    int extra = 25;
-    //value <= extra     extra < value      value > -25     value  >=  extra
-    System.out.print(value <= extra);
-    System.out.print(extra < value);
-    System.out.print(value > -25);
-    System.out.print(value  >=  extra);
+    // 2.Create An Array of integers from 1 to 5, Call an integer on array using index and print it.
+    int[] newArr = new int[]{1,2,3,4,5};
+    System.out.println(newArr[0]);
+
+    // 3.Call index outside of the bound. Handle this exception printing “Outside of bound message.”
+    try{
+      System.out.println(newArr[5]);
+    }
+    catch(Exception e) {
+      System.out.println("Outside of bound message.");
+    }
 
 
   }
